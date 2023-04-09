5877982d46d997898fd7e9b5b68aa71a3f148146
OzawaRio <ozawa-rio@mail.nissan.co.jp>
2023-04-09 21:25:34 +0900

    Futureは待たれる側につける(次の処理を実施する)、async&awaitは待つ側が設定する。

--- day230409/prac_async.dart
+++ day230409/prac_async.dart
@@ -1,5 +1,5 @@
 prac_async() {
-  Test2.start();
+  Test3.start();
 }
 
 //実験１
@@ -54,5 +54,35 @@ class Test2 {
       i++;
     }
     return 20;
+  }
+}
+
+//実験３（実践的な構成？）
+//情報の取得時だけFutureを付与
+// 結果は以下
+// 情報を取得します
+// 情報取得中です
+// Test3 バッテリー残量は30%です
+// 情報取得中ですから２秒後にバッテリー情報取得
+// displayより後ろにあるloadingを処理していることからローディング処理が止まらずにデータの読み込みができる
+class Test3 {
+  static void start() {
+    print("情報を取得します");
+    display();
+    loading();
+  }
+
+  //バッテリー情報の表示
+  static void display() async {
+    int status = await fetch();
+    print("Test3 バッテリー残量は$status%です");
+  }
+
+  //待機中のUI（ローディング画面）
+  static void loading() => print("情報取得中です");
+
+  //情報の取得
+  static Future<int> fetch() {
+    return Future.delayed(Duration(seconds: 2), () => 30);
   }
 }
